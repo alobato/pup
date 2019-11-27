66e18b823cf785d67e89099dcd9840b0c0955110
Arley Lobato <arleylobato@gmail.com>
2019-11-27 17:50:58 -0300

    Adjust css reset

--- routes/pup.js
+++ routes/pup.js
@@ -46,11 +46,6 @@ html {
   font-family: sans-serif;
   box-sizing: border-box;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-html, body {
-  width: 100%;
-  height: 100%;
 }
 body {
   margin: 0;
