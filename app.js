0e8aaa00c67bc64784ffbb8624a6c34b02c5d665
Arley Lobato <arleylobato@gmail.com>
2019-12-20 13:37:35 -0300

    Remove cors. Only in nginx

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
-const cors = require('cors')
+// const cors = require('cors')
 
 const routes = require('./routes')
 
 const app = express()
 
-app.use(cors())
+// app.use(cors())
 
 app.use(morgan('combined'))
 
